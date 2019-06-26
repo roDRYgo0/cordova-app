95c2d7060270972fd6164beb129aaa3527f73d9a
Rodrigo Castillo <dev.rodrig@gmail.com>
2019-06-26 17:35:49 -0600

    first commit

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,50 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "pluginId": "cordova-plugin-camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
+        "id": "cordova-plugin-camera.CameraProxy",
+        "pluginId": "cordova-plugin-camera",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-navigationbar/www/navigationbar.js",
+        "id": "cordova-plugin-navigationbar.navigationbar",
+        "pluginId": "cordova-plugin-navigationbar",
+        "clobbers": [
+            "window.navigationbar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-camera": "4.0.3",
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-navigationbar": "1.0.31"
+}
+// BOTTOM OF METADATA
+});
